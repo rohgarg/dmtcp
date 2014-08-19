0efdff04b2c66f1c7f652b945ddb7dca02b16039
Rohan Garg <rhn.grg@gmail.com>
2014-08-19 14:57:33 -0400

    Update glx wrappers
     - Added more glx wrappers
     - Changed functions to extern "C"

--- test/plugin/xlib/src/xlib_plugin_glx_wrappers.cpp
+++ test/plugin/xlib/src/xlib_plugin_glx_wrappers.cpp
@@ -1,94 +1,139 @@
+#include <sstream>
+#include <cstring>
 #include "xlib_plugin_glx_wrappers.h"
+#include "xlib_plugin_virtualization.h"
 
 /* Auto generated code starts below */
-XVisualInfo* glXChooseVisual( Display *dpy, int screen,int *attribList ) {
+extern "C" XVisualInfo* glXChooseVisual (Display *dpy, int screen, int *attribList)
+{
   DPRINTF("glXChooseVisual()\n");
-  return _real_glXChooseVisual(dpy,screen,attribList);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  /* TODO: Check if this needs to be replayed:
+   *         - Do we need to save attribList (ends at None)
+   *         - Do we need to save the return value
+   */
+  return _real_glXChooseVisual(l_real_dp, screen, attribList);
 }
 
-GLXContext glXCreateContext( Display *dpy, XVisualInfo *vis,GLXContext shareList, Bool direct ) {
+extern "C" GLXContext glXCreateContext (Display *dpy, XVisualInfo *vis, GLXContext shareList, Bool direct)
+{
   DPRINTF("glXCreateContext()\n");
-  return _real_glXCreateContext(dpy,vis,shareList,direct);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  GLXContext l_real_shareList = XLIB_VIRT_CALL_FUNC.virtual_to_real_glxcontext(shareList);
+  XVisualInfo *l_vis_buff = NULL;
+  if (vis)
+  {
+    l_vis_buff = new XVisualInfo;
+    memcpy(l_vis_buff, vis, sizeof(XVisualInfo));
+  }
+  std::ostringstream funcCall;
+  funcCall << "glXCreateContext(" << (void*)dpy << "," << (void*)l_vis_buff << "," << shareList << "," << direct << ")";
+  XLIB_VIRT_SAVE_FUNC_CALL(funcCall.str());
+  GLXContext l_real_glxctx =  _real_glXCreateContext(l_real_dp,l_vis_buff,l_real_shareList,direct);
+  return XLIB_VIRT_CALL_FUNC.save_glxctx(l_real_glxctx);
 }
 
-void glXDestroyContext( Display *dpy, GLXContext ctx ) {
+extern "C" void glXDestroyContext (Display *dpy, GLXContext ctx)
+{
   DPRINTF("glXDestroyContext()\n");
-  _real_glXDestroyContext(dpy,ctx);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  GLXContext l_real_ctx = XLIB_VIRT_CALL_FUNC.virtual_to_real_glxcontext(ctx);
+  std::ostringstream funcCall;
+  funcCall << "glXDestroyContext(" << (void*)dpy << "," << ctx << ")";
+  XLIB_VIRT_SAVE_FUNC_CALL(funcCall.str());
+  _real_glXDestroyContext(l_real_dp,l_real_ctx);
 }
 
-Bool glXMakeCurrent( Display *dpy, GLXDrawable drawable,GLXContext ctx) {
+extern "C" Bool glXMakeCurrent (Display *dpy, GLXDrawable drawable, GLXContext ctx)
+{
   DPRINTF("glXMakeCurrent()\n");
-  return _real_glXMakeCurrent(dpy,drawable,ctx);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  GLXContext l_real_ctx = XLIB_VIRT_CALL_FUNC.virtual_to_real_glxcontext(ctx);
+  GLXDrawable l_real_d = XLIB_VIRT_CALL_FUNC.virtual_to_real_glxdrawable(drawable);
+  std::ostringstream funcCall;
+  funcCall << "glXMakeCurrent(" << (void*)dpy << "," << drawable << "," << ctx << ")";
+  XLIB_VIRT_SAVE_FUNC_CALL(funcCall.str());
+  return _real_glXMakeCurrent(l_real_dp,l_real_d,l_real_ctx);
 }
 
-void glXCopyContext( Display *dpy, GLXContext src, GLXContext dst,unsigned long mask ) {
+extern "C" void glXCopyContext( Display *dpy, GLXContext src, GLXContext dst,unsigned long mask ) {
   DPRINTF("glXCopyContext()\n");
   _real_glXCopyContext(dpy,src,dst,mask);
 }
 
-void glXSwapBuffers( Display *dpy, GLXDrawable drawable ) {
+extern "C" void glXSwapBuffers (Display *dpy, GLXDrawable drawable)
+{
   DPRINTF("glXSwapBuffers()\n");
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  GLXDrawable l_real_d = XLIB_VIRT_CALL_FUNC.virtual_to_real_glxdrawable(drawable);
+  std::ostringstream funcCall;
+  funcCall << "glXSwapBuffers(" << (void*)dpy << "," << drawable << ")";
+  XLIB_VIRT_SAVE_FUNC_CALL(funcCall.str());
   _real_glXSwapBuffers(dpy,drawable);
 }
 
-GLXPixmap glXCreateGLXPixmap( Display *dpy, XVisualInfo *visual,Pixmap pixmap ) {
+extern "C" GLXPixmap glXCreateGLXPixmap( Display *dpy, XVisualInfo *visual,Pixmap pixmap ) {
   DPRINTF("glXCreateGLXPixmap()\n");
   return _real_glXCreateGLXPixmap(dpy,visual,pixmap);
 }
 
-void glXDestroyGLXPixmap( Display *dpy, GLXPixmap pixmap ) {
+extern "C" void glXDestroyGLXPixmap( Display *dpy, GLXPixmap pixmap ) {
   DPRINTF("glXDestroyGLXPixmap()\n");
   _real_glXDestroyGLXPixmap(dpy,pixmap);
 }
 
-Bool glXQueryExtension( Display *dpy, int *errorb, int *event ) {
+extern "C" Bool glXQueryExtension( Display *dpy, int *errorb, int *event ) {
   DPRINTF("glXQueryExtension()\n");
   return _real_glXQueryExtension(dpy,errorb,event);
 }
 
-Bool glXQueryVersion( Display *dpy, int *maj, int *min ) {
+extern "C" Bool glXQueryVersion (Display *dpy, int *maj, int *min)
+{
   DPRINTF("glXQueryVersion()\n");
-  return _real_glXQueryVersion(dpy,maj,min);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  return _real_glXQueryVersion(l_real_dp,maj,min);
 }
 
-Bool glXIsDirect( Display *dpy, GLXContext ctx ) {
+extern "C" Bool glXIsDirect( Display *dpy, GLXContext ctx ) {
   DPRINTF("glXIsDirect()\n");
   return _real_glXIsDirect(dpy,ctx);
 }
 
-int glXGetConfig( Display *dpy, XVisualInfo *visual,int attrib, int *value ) {
+extern "C" int glXGetConfig( Display *dpy, XVisualInfo *visual,int attrib, int *value ) {
   DPRINTF("glXGetConfig()\n");
   return _real_glXGetConfig(dpy,visual,attrib,value);
 }
 
-GLXContext glXGetCurrentContext( void ) {
+extern "C" GLXContext glXGetCurrentContext( void ) {
   DPRINTF("glXGetCurrentContext()\n");
   return _real_glXGetCurrentContext();
 }
 
-GLXDrawable glXGetCurrentDrawable( void ) {
+extern "C" GLXDrawable glXGetCurrentDrawable( void ) {
   DPRINTF("glXGetCurrentDrawable()\n");
   return _real_glXGetCurrentDrawable();
 }
 
-void glXWaitGL( void ) {
+extern "C" void glXWaitGL( void ) {
   DPRINTF("glXWaitGL()\n");
   _real_glXWaitGL();
 }
 
-void glXWaitX( void ) {
+extern "C" void glXWaitX( void ) {
   DPRINTF("glXWaitX()\n");
   _real_glXWaitX();
 }
 
-void glXUseXFont( Font font, int first, int count, int list ) {
+extern "C" void glXUseXFont( Font font, int first, int count, int list ) {
   DPRINTF("glXUseXFont()\n");
   _real_glXUseXFont(font,first,count,list);
 }
 
-const char *glXQueryExtensionsString( Display *dpy, int screen ) {
+const char *glXQueryExtensionsString (Display *dpy, int screen)
+{
   DPRINTF("glXQueryExtensionsString()\n");
-  return _real_glXQueryExtensionsString(dpy,screen);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  return _real_glXQueryExtensionsString(l_real_dp,screen);
 }
 
 const char *glXQueryServerString( Display *dpy, int screen, int name ) {
@@ -106,87 +151,93 @@ Display *glXGetCurrentDisplay( void ) {
   return _real_glXGetCurrentDisplay();
 }
 
-GLXFBConfig *glXChooseFBConfig( Display *dpy, int screen,const int *attribList, int *nitems ) {
+extern "C" GLXFBConfig *glXChooseFBConfig (Display *dpy, int screen, const int *attribList, int *nitems)
+{
   DPRINTF("glXChooseFBConfig()\n");
-  return _real_glXChooseFBConfig(dpy,screen,attribList,nitems);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  return _real_glXChooseFBConfig(l_real_dp,screen,attribList,nitems);
 }
 
-int glXGetFBConfigAttrib( Display *dpy, GLXFBConfig config,int attribute, int *value ) {
+extern "C" int glXGetFBConfigAttrib (Display *dpy, GLXFBConfig config, int attribute, int *value)
+{
   DPRINTF("glXGetFBConfigAttrib()\n");
-  return _real_glXGetFBConfigAttrib(dpy,config,attribute,value);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  return _real_glXGetFBConfigAttrib(l_real_dp,config,attribute,value);
 }
 
-GLXFBConfig *glXGetFBConfigs( Display *dpy, int screen,int *nelements ) {
+extern "C" GLXFBConfig *glXGetFBConfigs( Display *dpy, int screen,int *nelements ) {
   DPRINTF("glXGetFBConfigs()\n");
   return _real_glXGetFBConfigs(dpy,screen,nelements);
 }
 
-XVisualInfo *glXGetVisualFromFBConfig( Display *dpy,GLXFBConfig config ) {
+extern "C" XVisualInfo *glXGetVisualFromFBConfig (Display *dpy, GLXFBConfig config)
+{
   DPRINTF("glXGetVisualFromFBConfig()\n");
-  return _real_glXGetVisualFromFBConfig(dpy,config);
+  Display *l_real_dp = XLIB_VIRT_CALL_FUNC.virtual_to_real_display_pointer(dpy);
+  return _real_glXGetVisualFromFBConfig(l_real_dp,config);
 }
 
-GLXWindow glXCreateWindow( Display *dpy, GLXFBConfig config,Window win, const int *attribList ) {
+extern "C" GLXWindow glXCreateWindow( Display *dpy, GLXFBConfig config,Window win, const int *attribList ) {
   DPRINTF("glXCreateWindow()\n");
   return _real_glXCreateWindow(dpy,config,win,attribList);
 }
 
-void glXDestroyWindow( Display *dpy, GLXWindow window ) {
+extern "C" void glXDestroyWindow( Display *dpy, GLXWindow window ) {
   DPRINTF("glXDestroyWindow()\n");
   _real_glXDestroyWindow(dpy,window);
 }
 
-GLXPixmap glXCreatePixmap( Display *dpy, GLXFBConfig config,Pixmap pixmap, const int *attribList ) {
+extern "C" GLXPixmap glXCreatePixmap( Display *dpy, GLXFBConfig config,Pixmap pixmap, const int *attribList ) {
   DPRINTF("glXCreatePixmap()\n");
   return _real_glXCreatePixmap(dpy,config,pixmap,attribList);
 }
 
-void glXDestroyPixmap( Display *dpy, GLXPixmap pixmap ) {
+extern "C" void glXDestroyPixmap( Display *dpy, GLXPixmap pixmap ) {
   DPRINTF("glXDestroyPixmap()\n");
   _real_glXDestroyPixmap(dpy,pixmap);
 }
 
-GLXPbuffer glXCreatePbuffer( Display *dpy, GLXFBConfig config,const int *attribList ) {
+extern "C" GLXPbuffer glXCreatePbuffer( Display *dpy, GLXFBConfig config,const int *attribList ) {
   DPRINTF("glXCreatePbuffer()\n");
   return _real_glXCreatePbuffer(dpy,config,attribList);
 }
 
-void glXDestroyPbuffer( Display *dpy, GLXPbuffer pbuf ) {
+extern "C" void glXDestroyPbuffer( Display *dpy, GLXPbuffer pbuf ) {
   DPRINTF("glXDestroyPbuffer()\n");
   _real_glXDestroyPbuffer(dpy,pbuf);
 }
 
-void glXQueryDrawable( Display *dpy, GLXDrawable draw, int attribute,unsigned int *value ) {
+extern "C" void glXQueryDrawable( Display *dpy, GLXDrawable draw, int attribute,unsigned int *value ) {
   DPRINTF("glXQueryDrawable()\n");
   _real_glXQueryDrawable(dpy,draw,attribute,value);
 }
 
-GLXContext glXCreateNewContext( Display *dpy, GLXFBConfig config,int renderType, GLXContext shareList,Bool direct ) {
+extern "C" GLXContext glXCreateNewContext( Display *dpy, GLXFBConfig config,int renderType, GLXContext shareList,Bool direct ) {
   DPRINTF("glXCreateNewContext()\n");
   return _real_glXCreateNewContext(dpy,config,renderType,shareList,direct);
 }
 
-Bool glXMakeContextCurrent( Display *dpy, GLXDrawable draw,GLXDrawable read, GLXContext ctx ) {
+extern "C" Bool glXMakeContextCurrent( Display *dpy, GLXDrawable draw,GLXDrawable read, GLXContext ctx ) {
   DPRINTF("glXMakeContextCurrent()\n");
   return _real_glXMakeContextCurrent(dpy,draw,read,ctx);
 }
 
-GLXDrawable glXGetCurrentReadDrawable( void ) {
+extern "C" GLXDrawable glXGetCurrentReadDrawable( void ) {
   DPRINTF("glXGetCurrentReadDrawable()\n");
   return _real_glXGetCurrentReadDrawable();
 }
 
-int glXQueryContext( Display *dpy, GLXContext ctx, int attribute,int *value ) {
+extern "C" int glXQueryContext( Display *dpy, GLXContext ctx, int attribute,int *value ) {
   DPRINTF("glXQueryContext()\n");
   return _real_glXQueryContext(dpy,ctx,attribute,value);
 }
 
-void glXSelectEvent( Display *dpy, GLXDrawable drawable,unsigned long mask ) {
+extern "C" void glXSelectEvent( Display *dpy, GLXDrawable drawable,unsigned long mask ) {
   DPRINTF("glXSelectEvent()\n");
   _real_glXSelectEvent(dpy,drawable,mask);
 }
 
-void glXGetSelectedEvent( Display *dpy, GLXDrawable drawable,unsigned long *mask ) {
+extern "C" void glXGetSelectedEvent( Display *dpy, GLXDrawable drawable,unsigned long *mask ) {
   DPRINTF("glXGetSelectedEvent()\n");
   _real_glXGetSelectedEvent(dpy,drawable,mask);
 }
@@ -196,72 +247,72 @@ __GLXextFuncPtr glXGetProcAddressARB (const GLubyte *name) {
   return _real_glXGetProcAddressARB(name);
 }
 
-void (*glXGetProcAddress(const GLubyte *procname))( void ) {
+extern "C" void (*glXGetProcAddress(const GLubyte *procname))( void ) {
   DPRINTF("glXGetProcAddress(%s)\n", procname);
   return _real_glXGetProcAddress(procname);
 }
 
-void *glXAllocateMemoryNV(GLsizei size, GLfloat readfreq, GLfloat writefreq, GLfloat priority) {
+extern "C" void *glXAllocateMemoryNV(GLsizei size, GLfloat readfreq, GLfloat writefreq, GLfloat priority) {
   DPRINTF("glXAllocateMemoryNV()\n");
   return _real_glXAllocateMemoryNV(size,readfreq,writefreq,priority);
 }
 
-void glXFreeMemoryNV(GLvoid *pointer) {
+extern "C" void glXFreeMemoryNV(GLvoid *pointer) {
   DPRINTF("glXFreeMemoryNV()\n");
   _real_glXFreeMemoryNV(pointer);
 }
 
-Bool glXBindTexImageARB(Display *dpy, GLXPbuffer pbuffer, int buffer) {
+extern "C" Bool glXBindTexImageARB(Display *dpy, GLXPbuffer pbuffer, int buffer) {
   DPRINTF("glXBindTexImageARB()\n");
   return _real_glXBindTexImageARB(dpy,pbuffer,buffer);
 }
 
-Bool glXReleaseTexImageARB(Display *dpy, GLXPbuffer pbuffer, int buffer) {
+extern "C" Bool glXReleaseTexImageARB(Display *dpy, GLXPbuffer pbuffer, int buffer) {
   DPRINTF("glXReleaseTexImageARB()\n");
   return _real_glXReleaseTexImageARB(dpy,pbuffer,buffer);
 }
 
-Bool glXDrawableAttribARB(Display *dpy, GLXDrawable draw, const int *attribList) {
+extern "C" Bool glXDrawableAttribARB(Display *dpy, GLXDrawable draw, const int *attribList) {
   DPRINTF("glXDrawableAttribARB()\n");
   return _real_glXDrawableAttribARB(dpy,draw,attribList);
 }
 
-int glXGetFrameUsageMESA(Display *dpy, GLXDrawable drawable, float *usage) {
+extern "C" int glXGetFrameUsageMESA(Display *dpy, GLXDrawable drawable, float *usage) {
   DPRINTF("glXGetFrameUsageMESA()\n");
   return _real_glXGetFrameUsageMESA(dpy,drawable,usage);
 }
 
-int glXBeginFrameTrackingMESA(Display *dpy, GLXDrawable drawable) {
+extern "C" int glXBeginFrameTrackingMESA(Display *dpy, GLXDrawable drawable) {
   DPRINTF("glXBeginFrameTrackingMESA()\n");
   return _real_glXBeginFrameTrackingMESA(dpy,drawable);
 }
 
-int glXEndFrameTrackingMESA(Display *dpy, GLXDrawable drawable) {
+extern "C" int glXEndFrameTrackingMESA(Display *dpy, GLXDrawable drawable) {
   DPRINTF("glXEndFrameTrackingMESA()\n");
   return _real_glXEndFrameTrackingMESA(dpy,drawable);
 }
 
-int glXQueryFrameTrackingMESA(Display *dpy, GLXDrawable drawable, int64_t *swapCount, int64_t *missedFrames, float *lastMissedUsage) {
+extern "C" int glXQueryFrameTrackingMESA(Display *dpy, GLXDrawable drawable, int64_t *swapCount, int64_t *missedFrames, float *lastMissedUsage) {
   DPRINTF("glXQueryFrameTrackingMESA()\n");
   return _real_glXQueryFrameTrackingMESA(dpy,drawable,swapCount,missedFrames,lastMissedUsage);
 }
 
-int glXSwapIntervalMESA(unsigned int interval) {
+extern "C" int glXSwapIntervalMESA(unsigned int interval) {
   DPRINTF("glXSwapIntervalMESA()\n");
   return _real_glXSwapIntervalMESA(interval);
 }
 
-int glXGetSwapIntervalMESA(void) {
+extern "C" int glXGetSwapIntervalMESA(void) {
   DPRINTF("glXGetSwapIntervalMESA()\n");
   return _real_glXGetSwapIntervalMESA();
 }
 
-void glXBindTexImageEXT(Display *dpy, GLXDrawable drawable, int buffer, const int *attrib_list) {
+extern "C" void glXBindTexImageEXT(Display *dpy, GLXDrawable drawable, int buffer, const int *attrib_list) {
   DPRINTF("glXBindTexImageEXT()\n");
   _real_glXBindTexImageEXT(dpy,drawable,buffer,attrib_list);
 }
 
-void glXReleaseTexImageEXT(Display *dpy, GLXDrawable drawable, int buffer) {
+extern "C" void glXReleaseTexImageEXT(Display *dpy, GLXDrawable drawable, int buffer) {
   DPRINTF("glXReleaseTexImageEXT()\n");
   _real_glXReleaseTexImageEXT(dpy,drawable,buffer);
 }
